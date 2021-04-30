4b3eb892b64361059fe80a4b3b8dc64aa48a1848
Anthony Rommanus <thonyromes@gmail.com>
2021-04-30 13:48:11 +0100

    Add test file

--- src/utils/mergeCoins.js
+++ src/utils/mergeCoins.js
@@ -1,27 +1,49 @@
-// prettier-ignore
-const mergeCoins = (list) => list.reduce((acc, cur) => {
-  let accumulator = acc;
-  // Get the index of the key-value pair.
-  const occursIndex = accumulator.reduce(
-    (n, item, i) => (item['coin/token'] === cur['coin/token'] ? i : n),
-    -1,
-  );
+// const mergeCoins = (list) => list.reduce((acc, cur) => {
+//   let accumulator = acc;
+//   // Get the index of the key-value pair.
+//   const occursIndex = accumulator.reduce(
+//     (n, item, i) => (item['coin/token'] === cur['coin/token'] ? i : n),
+//     -1,
+//   );
 
-  // If the name is found,
-  if (occursIndex >= 0) {
+//   // If the name is found,
+//   if (occursIndex >= 0) {
+//     // prettier-ignore
+//     // add the current unit/cost to previous occurrences' units/cost.
+//     accumulator[occursIndex].unit = Number(accumulator[occursIndex].unit) + Number(cur.unit);
+//     accumulator[occursIndex]['total cost'] = Number(accumulator[occursIndex]['total cost'])
+//       + Number(cur['total cost']);
+
+//     // Otherwise,
+//   } else {
+//     // add the current item to the accumulator
+//     accumulator = [...accumulator, cur];
+//   }
+
+//   return accumulator;
+// }, []);
+
+const mergeCoins = (list) => {
+  const coins = [...list];
+
+  const merge = {};
+
+  let obj;
+
+  coins.forEach((coin) => {
+    obj = merge[coin['coin/token']] || {};
+
     // prettier-ignore
-    // add the current unit/cost to previous occurrences' units/cost.
-    accumulator[occursIndex].unit = Number(accumulator[occursIndex].unit) + Number(cur.unit);
-    accumulator[occursIndex]['total cost'] = Number(accumulator[occursIndex]['total cost'])
-      + Number(cur['total cost']);
+    if (obj['coin/token'] === coin['coin/token']) {
+      merge[coin['coin/token']].unit = Number(merge[coin['coin/token']].unit) + Number(coin.unit);
+      merge[coin['coin/token']]['total cost'] = Number(merge[coin['coin/token']]['total cost'])
+        + Number(coin['total cost']);
+    } else {
+      merge[coin['coin/token']] = coin;
+    }
+  });
 
-    // Otherwise,
-  } else {
-    // add the current item to the accumulator
-    accumulator = [...accumulator, cur];
-  }
-
-  return accumulator;
-}, []);
+  return Object.values(merge);
+};
 
 export default mergeCoins;
---
+++ test.js
@@ -0,0 +1,51 @@
+const t = {
+  ADA: {
+    'coin/token': 'ADA',
+    percentage_to_sell_at: '',
+    'purchase id': '9',
+    'total cost': '800',
+    unit: '1000',
+  },
+  BTC: {
+    'coin/token': 'BTC',
+    percentage_to_sell_at: '50',
+    'purchase id': '7',
+    'total cost': '13000',
+    unit: '1',
+  },
+  DOT: {
+    'coin/token': 'DOT',
+    percentage_to_sell_at: '80',
+    'purchase id': '8',
+    'total cost': '400',
+    unit: '4',
+  },
+  ETH: {
+    'coin/token': 'ETH',
+    percentage_to_sell_at: '25',
+    'purchase id': '1',
+    'total cost': '780',
+    unit: '0.270.5',
+  },
+  GAS: {
+    'coin/token': 'GAS',
+    percentage_to_sell_at: '',
+    'purchase id': '2',
+    'total cost': '455',
+    unit: '3010',
+  },
+  OXT: {
+    'coin/token': 'OXT',
+    percentage_to_sell_at: '',
+    'purchase id': '5',
+    'total cost': '1200',
+    unit: '1000',
+  },
+  VET: {
+    'coin/token': 'VET',
+    percentage_to_sell_at: '',
+    'purchase id': '3',
+    'total cost': '45',
+    unit: '500',
+  },
+};
